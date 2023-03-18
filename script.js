e4c6580f2affb230a696a510ec33b198cb4ebf3f
mruga-IT <124659354+mruga-IT@users.noreply.github.com>
2023-03-18 13:05:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 //complete the given function
-
-function palindrome(str){
- str.toLowerCase();
- let verdict  = true;
-	for(let i = 0;i<str.length;i++){
-		for( let j = str.length;j>0;j--){
-			if(String[i]!==String[j]){
-				verdict = false;
-			}
-		}
-	}
-	return verdict;
-}
- const string = str.toLowerCase();
- const len = str.length;
-
-    // loop through half of the string
-    for (let i = 0; i < len / 2; i++) {
-
-        // check if first and last string are same
-        if (string[i] !== string[len - 1 - i]) {
-            return false;
-        }
+function isPalindrome(s) {
+  // Convert the string to lowercase and remove non-alphanumeric characters
+  s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+  
+  // Check if the string is the same forward and backward
+  for (let i = 0, j = s.length - 1; i < j; i++, j--) {
+    if (s[i] !== s[j]) {
+      return false;
     }
-    return true;
+  }
+  
+  return true;
 }
-module.exports = palindrome
+This function first converts the input string to lowercase and removes any non-alphanumeric characters using a regular expression. It then checks if the resulting string is a palindrome by iterating over the characters from both ends of the string and comparing them. If at any point the characters are different, the function returns false. If the loop completes without finding any differences, the function returns true.
+
+For example, isPalindrome("race a car") would return false because the string "raceacar" is not a palindrome.
+
+
+
+
+
+
+
+
+
