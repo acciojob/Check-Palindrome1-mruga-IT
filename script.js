307c31d1c1a485b68c1913532ff99a01b5e66e4c
mruga-IT <124659354+mruga-IT@users.noreply.github.com>
2023-03-18 13:03:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 complete the given function
 
 function palindrome(str){
-//  str.toLowerCase();
-//  let verdict  = true;
-// 	for(let i = 0;i<str.length;i++){
-// 		for( let j = str.length;j>0;j--){
-// 			if(String[i]!==String[j]){
-// 				verdict = false;
-// 			}
-// 		}
-// 	}
-// 	return verdict;
-// }
+ str.toLowerCase();
+ let verdict  = true;
+	for(let i = 0;i<str.length;i++){
+		for( let j = str.length;j>0;j--){
+			if(String[i]!==String[j]){
+				verdict = false;
+			}
+		}
+	}
+	return verdict;
+}
  const string = str.toLowerCase();
  const len = str.length;
 
